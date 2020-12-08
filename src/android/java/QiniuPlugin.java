d84b783983fdc6d1b9ba24b63c03efce7501a422
max <max_meng@yeah.net>
2020-12-09 01:18:14 +0800

    done upload plugin

--- src/android/java/QiniuPlugin.java
+++ src/android/java/QiniuPlugin.java
@@ -9,6 +9,7 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -26,84 +27,101 @@ import android.util.Log;
 
 /**
  * @author yumemor
- * 
- *         基于七牛云储存编写的Corodva上传插件
+ * <p>
+ * 基于七牛云储存编写的Corodva上传插件
  */
 public class QiniuPlugin extends CordovaPlugin implements UpCompletionHandler {
-	/**
-	 * 七牛上传管理器
-	 */
-	private UploadManager uploadManager;
-	/**
-	 * cordova回调
-	 */
-	private CallbackContext callbackContext;
-	
-	@Override
-	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-		uploadManager = new UploadManager();
-	}
+    /**
+     * 七牛上传管理器
+     */
+    private UploadManager uploadManager;
+    /**
+     * cordova回调
+     */
+    private CallbackContext callbackContext;
 
-	@Override
-	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		this.callbackContext = callbackContext;
-		try {
-			Method method = this.getClass().getDeclaredMethod(action,JSONArray.class);
-			if (method != null) {
-				method.invoke(this, args);
-				return true;
-			} else {
-				callbackContext.error("Action错误!");
-			}
-		} catch (NoSuchMethodException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
-		}
-		return false;
-	}
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        uploadManager = new UploadManager();
+    }
 
-	private void uploadFile(JSONArray args) throws JSONException,UnsupportedEncodingException {
-		// String prefix = args.optJSONObject(0).getString("prefix");
-		// String filePath = args.optJSONObject(0).getString("filePath");
-		// String name = StrUtils.appendPrefix(prefix, StrUtils.getFileName(filePath));	//获取文件名称 添加前缀
-        // args: [filePath, remoteKey, token, callbacks.progressCbk, options]);
-		String filePath = args.optString(0);
-		String remoteKey = args.optString(1);
-		String token = args.optString(2);
-		// JSONObject progressCbk = args.optJSONObject(3);
-		 JSONObject options = args.optJSONObject(4);
-		UploadOptions uploadOptions = new UploadOptions(null, null, options.optBoolean("checkCrc"), new UpProgressHandler(){
-            public void progress(String key, double percent){
-				Log.i("qiniu", key + ": " + percent);
-				// todo progress cbk
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        this.callbackContext = callbackContext;
+        try {
+            Method method = this.getClass().getDeclaredMethod(action, JSONArray.class);
+            if (method != null) {
+                method.invoke(this, args);
+                return true;
+            } else {
+                callbackContext.error("Action错误!");
             }
-        }, null); // todo can't cancel
-		filePath = URLDecoder.decode(filePath, "UTF-8");	//文件路径解码
-		filePath = filePath.replace("file://","");	//去掉 file:// 路径。
-		uploadManager.put(filePath, remoteKey, token, this, uploadOptions);
-	}
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
-	@Override
-	public void complete(String name, ResponseInfo info, JSONObject json) {
-		int status = info.statusCode;
-		JSONObject jsonObject = new JSONObject();
-		try {
-			jsonObject.put("name", name);
-			jsonObject.put("status", status);
-			jsonObject.put("msg", info.error);
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-		if (status == 200) {
-			callbackContext.success(jsonObject);
-		} else {
-			callbackContext.error(jsonObject);
-		}
-	}
+    private void uploadFile(JSONArray args) throws JSONException, UnsupportedEncodingException {
+        // String prefix = args.optJSONObject(0).getString("prefix");
+        // String filePath = args.optJSONObject(0).getString("filePath");
+        // String name = StrUtils.appendPrefix(prefix, StrUtils.getFileName(filePath));	//获取文件名称 添加前缀
+        // args: [filePath, remoteKey, token, callbacks.progressCbk, options]);
+        String filePath = args.optString(0);
+        String remoteKey = args.optString(1);
+        String token = args.optString(2);
+        JSONObject options = args.optJSONObject(3);
+        final double[] lastPercent = {0};
+        UploadOptions uploadOptions = new UploadOptions(null,
+                options.optString("mimeType"), options.optBoolean("checkCrc"),
+                (key, percent) -> {
+                    if (percent - lastPercent[0] > 0.1) {
+                        Log.i("qiniu", key + ": " + percent);
+                    }
+                    lastPercent[0] = percent;
+                    JSONObject rspJson = new JSONObject();
+                    try {
+                        rspJson.put("type", "progress");
+                        rspJson.put("data", percent);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    PluginResult prs = new PluginResult(PluginResult.Status.OK, rspJson);
+                    prs.setKeepCallback(true);
+                    callbackContext.sendPluginResult(prs);
+                }, null); // todo can't cancel
+        filePath = URLDecoder.decode(filePath, "UTF-8");    //文件路径解码
+        filePath = filePath.replace("file://", "");    //去掉 file:// 路径。
+        uploadManager.put(filePath, remoteKey, token, this, uploadOptions);
+    }
+
+    @Override
+    public void complete(String name, ResponseInfo info, JSONObject json) {
+        int statusCode = info.statusCode;
+        JSONObject jsonObject = new JSONObject();
+        try {
+            JSONObject httpObj = new JSONObject();
+            httpObj.put("statusCode", statusCode);
+            httpObj.put("message", info.message);
+            httpObj.put("error", info.error);
+
+            jsonObject.put("type", "success");
+            jsonObject.put("data", json);
+            jsonObject.put("response", httpObj);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        if (statusCode == 200) {
+            callbackContext.success(jsonObject);
+        } else {
+            callbackContext.error(jsonObject);
+        }
+    }
 
 }
--- www/qiniu.js
+++ www/qiniu.js
@@ -3,8 +3,17 @@ var cordova = require('cordova');
 
 var qiniu = {
     upload: function (filePath, remoteKey, token, callbacks, options) {
-        cordova.exec(callbacks.successCbk, callbacks.errorCbk, 'upload', 'uploadFile',
-            [filePath, remoteKey, token, callbacks.progressCbk, options]);
+
+        function successOrProgressCbk(rsp) {
+            if (rsp.type === 'progress') {
+                callbacks.progressCbk(rsp);
+            } else {
+                callbacks.successCbk(rsp);
+            }
+        }
+
+        cordova.exec(successOrProgressCbk, callbacks.errorCbk, 'upload', 'uploadFile',
+            [filePath, remoteKey, token, options]);
     }
 }
 
